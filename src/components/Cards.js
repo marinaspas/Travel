9782768fe335ba770b7a7d2e59e3e86dea48ab74
Marina <marinaspas@rina-mac.local>
2021-01-08 13:57:26 -0500

    trying to fix weird issues with my images

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,37 +10,37 @@ function Cards() {
         <div className="cards_wrapper">
           <ul className="cards__items">
             <CardItem 
-              src="images/maldives.jpg"
+              src="./images/maldives.jpg"
               text="Explore the serenity of the Maldives"
               label="Tropical"
               path="/services"
             />
             <CardItem 
-              src="images/paris.jpg"
+              src="../images/paris.jpg"
               text="Dusk at the Arc de Triomphe"
               label="Europe"
               path="/services"
             />
             <CardItem 
-              src="images/spain.jpg"
+              src="../images/spain.jpg"
               text="Walking around the narrow streets of Spain"
               label="Europe"
               path="/services"
             />
             <CardItem 
-              src="images/switzerland.jpg"
+              src="../images/switzerland.jpg"
               text="Taking in the views of the Swiss Mountains"
               label="Europe"
               path="/services"
             />
             <CardItem 
-              src="images/austria.jpg"
+              src="../images/austria.jpg"
               text="Wandering the streets of Vienna"
               label="Europe"
               path="/services"
             />
             <CardItem 
-              src="images/sydney.jpg"
+              src="../images/sydney.jpg"
               text="A night at the Sydney Opera"
               label="Oceania"
               path="/services"
